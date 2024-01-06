26e0e6c1bcfde26963f40b4639da2da2f9ce7357
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-01-05 19:33:27 -0500

    Add opacity animation for mouse icon

--- src/app/components/ScrollAnimation.tsx
+++ src/app/components/ScrollAnimation.tsx
@@ -1,28 +1,41 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { BsMouseFill } from "react-icons/bs";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const ScrollAnimation = () => {
-  const boxRef = useRef(null);
-  const mouseIconRef = useRef(null);
+  const boxRef = useRef<HTMLDivElement | null>(null);
+  const mouseIconRef = useRef<HTMLElement | null>(null);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const calculateMouseIconOpacity = (scrollTop: number) => {
+    if (scrollTop >= 100) return 0;
+    const rawOpacity = scrollTop / 100;
+    return 1 - Math.round(rawOpacity * 100) / 100;
+  };
 
   return (
     <div
       ref={boxRef}
       className="lg:w-2/3 md:w-1/2 text-gray-500 text-center overflow-y-scroll min-h-full h-full"
+      onScroll={() => setScrollTop(boxRef.current?.scrollTop || 0)}
     >
       <div className="min-h-full flex flex-col justify-center items-center relative px-40 gap-2">
         <p className="text-justify">
           Hello! My name is Ryan and I&apos;m a software developer with
           experience creating web applications. I'm a passionate and obsessive
-          builder with a strong work ethic and a love of learning.
+          builder with a strong work ethic and a deep love of learning.
         </p>
         <p className="text-justify">
           When I'm not coding, I&apos;m usually catching the game or watching a
           movie. Currently, I&apos;m studying to get my AWS (DVA-C02)
           certification.
         </p>
-        <span className="absolute left-1/2 bottom-16 flex flex-col justify-center items-center gap-2">
+        <span
+          className="absolute left-1/2 bottom-16 flex flex-col justify-center items-center gap-2 ease-in-out select-none"
+          style={{ opacity: calculateMouseIconOpacity(scrollTop) }}
+        >
           <span ref={mouseIconRef} className="animate-bounce" id="mouseIcon">
             <BsMouseFill size={30} />
           </span>
